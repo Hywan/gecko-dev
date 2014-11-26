2760a227933af6101f990b3efe52bdb30bb6cb43
Paul Rouget <me@paulrouget.com>
2014-11-26 19:05:08 +0100

    fix DMG build on mac

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,11 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if test "$MOZ_HTMLRUNNER" = "1"; then
-  MOZ_APP_BASENAME=Firefox.html
-else
-  MOZ_APP_BASENAME=Firefox
-fi
+MOZ_APP_BASENAME=Firefox
 
 MOZ_APP_VENDOR=Mozilla
 MOZ_UPDATER=1
